7a026fc25f5f568e2d0053e46272c31544042711
Ubuntu <jose.tanquilut@gmail.com>
2018-12-31 01:06:24 +0000

    Finished Phase II of kpf

--- ruby/data_structures/knightpathfinder.rb
+++ ruby/data_structures/knightpathfinder.rb
@@ -1,11 +1,23 @@
 require_relative "./skeleton/lib/00_tree_node.rb"
 
+# override default inspect behavior to examine move tree
+class PolyTreeNode
+    def inspect
+        if @children.length == 0
+            @value.inspect
+        else
+            @children
+        end
+    end
+end
+
 class KnightPathFinder
-    attr_reader :root_node, :considered_positions
+    attr_reader :root_node, :considered_positions, :move_tree
 
     def initialize(starting_position)
         @root_node = PolyTreeNode.new(starting_position)
         @considered_positions = Set[starting_position]
+        @move_tree = self.build_move_tree
     end
 
     def self.valid_moves(pos)
@@ -31,5 +43,26 @@ class KnightPathFinder
         end
 
         possible_positions.each { |ele| self.considered_positions << ele }
+    end
+
+    def build_move_tree
+        move_tree = self.root_node
+
+        # bfs algorithm to build the move tree
+        queue = [move_tree]
+
+        until queue.empty?
+            head = queue.shift
+
+            # add children to the queue to explore
+            # will eventually terminate due to considered_positions
+            self.new_move_positions(head.value).each do |pos|
+                new_child = PolyTreeNode.new(pos)
+                head.add_child(new_child)
+                queue << new_child
+            end
+        end
+
+        move_tree
     end
 end
