6e6f64942d7bd2d5c3852b116792cca8c98637d0
jtanquil <jose.tanquilut@gmail.com>
2019-01-03 18:01:46 -0800

    Completed Board.make_move

--- ruby/oop/mancala/skeleton/lib/board.rb
+++ ruby/oop/mancala/skeleton/lib/board.rb
@@ -1,3 +1,5 @@
+require "byebug"
+
 class Board
   attr_accessor :cups
 
@@ -22,31 +24,24 @@ class Board
   end
 
   def make_move(start_pos, current_player_name)
-    array_pos = (start_pos <= 6) ? start_pos - 1 : start_pos
     player_score_cup = (current_player_name == @name1) ? 6 : 13
-    stones = self.cups[array_pos].length
-    self.cups[array_pos] = []
-    indices = get_indices(stones, array_pos, player_score_cup)
+    stones = self.cups[start_pos] ? self.cups[start_pos].length : 0
+    # debugger
+    self.cups[start_pos] = []
+    indices = get_indices(stones, start_pos, player_score_cup)
 
     indices.each do |idx|
-      self.cups[indices] << :stone
+      self.cups[idx] << :stone
     end
 
-    # 3 cases for the last index:
-    # 1) current player's cup => pick another starting position
-    # 2) a nonempty cup => go again
-    # 3) an empty cup => switch players
-    if indices.last == player_score_cup
-      return :prompt
-    elsif self.cups[indices.last].length > 1
-      return indices.last
-    else
-      return :switch
-    end
+    render
+
+    next_turn(indices.last, player_score_cup)
   end
 
   # return indices; skip the opponent's score cup
   def get_indices(stones, array_pos, player_score_cup)
+    # debugger
     opp_score_cup = (player_score_cup == 6) ? 13 : 6
     offset = 1
     count = 0
@@ -54,7 +49,7 @@ class Board
 
     until count == stones
       if array_pos + offset != opp_score_cup
-        indices << array_pos + offset
+        indices << (array_pos + offset) % self.cups.length
         count += 1
       end
 
@@ -64,8 +59,18 @@ class Board
     indices
   end
 
-  def next_turn(ending_cup_idx)
-    # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
+  def next_turn(ending_cup_idx, player_score_cup)
+  # 3 cases for the last index:
+    # 1) current player's cup => pick another starting position
+    # 2) a nonempty cup => go again
+    # 3) an empty cup => switch players
+    if ending_cup_idx == player_score_cup
+      return :prompt
+    elsif self.cups[ending_cup_idx].length > 1
+      return ending_cup_idx
+    elsif self.cups[ending_cup_idx].length == 1
+      return :switch
+    end
   end
 
   def render
